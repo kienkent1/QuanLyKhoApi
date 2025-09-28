0f52ef6db2c195aea31692e15f1a2a88b986e638
kienkent1 <xuongkienphung@gmail.com>
2025-09-28 13:13:38 +0700

    Update-model-Service

--- QuanLyKhoApi/Controllers/HangHoaControlller.cs
+++ QuanLyKhoApi/Controllers/HangHoaControlller.cs
@@ -7,5 +7,9 @@ namespace QuanLyKhoApi.Controllers
     [ApiController]
     public class HangHoaControlller : ControllerBase
     {
+        public IActionResult GetHangHoa()
+        {
+   
+        }
     }
 }
---
+++ QuanLyKhoApi/Services/HangHoaService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using QuanLyKhoApi.Data;
+
+namespace QuanLyKhoApi.Services
+{
+    public class HangHoaService(AppDbContext db)
+    {
+        public List<HangHoa> GetHangHoaSV()
+        {
+            var hangHoas = db.HangHoa.ToList();
+            return hangHoas;
+        }
+    }
+}
